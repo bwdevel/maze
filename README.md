43e7d90063a37ac5ffbfbac4080fd5a4a6edd8d7
bwdevel <bwdevel@yandex.com>
2017-11-04 03:12:44 +0100

    cleanup and added README w/ links

---
+++ README.md
@@ -0,0 +1,9 @@
+# Maze Generator
+
+## Generate procedural mazes with Prim's algorithm
+
+Helpful links:
+
+* [Maze Generation on Wikipedia](https://en.wikipedia.org/wiki/Maze_generation_algorithm)
+* [Prim's Algo on StackOverflow](https://stackoverflow.com/questions/29739751/implementing-a-randomly-generated-maze-using-prims-algorithm)
+* [Prim's Algo on dev blog](http://weblog.jamisbuck.org/2011/1/10/maze-generation-prim-s-algorithm)
--- main.lua
+++ main.lua
@@ -1,35 +1,35 @@
 function love.load()
+  math.randomseed(os.time())
   debug = false
-  blocksize = 32
-  mazewidth = love.graphics.getWidth()/blocksize
-  mazeheight = love.graphics.getHeight()/blocksize
+  blocksize = 16
+  mazewidth = math.floor(love.graphics.getWidth()/blocksize)
+  mazeheight = math.floor(love.graphics.getHeight()/blocksize)
+
+  -- set maze globals and initial maze
+  counter = 0
+  maze = {}
   walllist = {}
-  maze = maze_generate(mazewidth, mazeheight)
-  -- find starting position
-  start = math.random(#maze)
-  maze[start].val = 0
-  maze[start].visited = true
-  --populate inital wall list
-  table_add_table(walllist, get_neighbors(start, 2, 1))
-  print(#walllist)
+  start = 0
+  maze_init()
 end
 
 function love.update(dt)
-    if #walllist > 0 then
-      local target = walllist[math.random(#walllist)]
-      local tneighbors = get_neighbors(target, 2, 0)
-      if #tneighbors > 0 then
-        local tneighbor = tneighbors[math.random(#tneighbors)]
-        local middle = get_middle(target, tneighbor)
-        maze[middle].val = 0
-        maze[target].val = 0
-        table_add_table(walllist, get_neighbors(target, 2, 1))
-        for i = #walllist, 1 , -1 do
-          if walllist[i] == target then table.remove(walllist, i) end
-        end
+  --if #walllist > 0 then
+  if #walllist == 0 then maze_init() end
+  while #walllist > 0 do
+    local target = walllist[math.random(#walllist)]
+    local tneighbors = get_neighbors(target, 2, 0)
+    if #tneighbors > 0 then
+      local tneighbor = tneighbors[math.random(#tneighbors)]
+      local middle = get_middle(target, tneighbor)
+      maze[middle].val = 0
+      maze[target].val = 0
+      table_add_table(walllist, get_neighbors(target, 2, 1))
+      for i = #walllist, 1 , -1 do
+        if walllist[i] == target then table.remove(walllist, i) end
       end
-    else
     end
+  end
 end
 
 function love.draw()
@@ -42,19 +42,21 @@ function love.keyreleased(key)
   if key == 'p' then
     if debug then debug = false else debug = true end
   end
+  if key == 'r' and #walllist == 0 then maze_init() end
 end
 
 function draw_debug()
   local x = 10
   local y = 10
-  local w = 100
+  local w = 150
   local h = 50
-  love.graphics.setColor(0, 0, 0, 128)
+  love.graphics.setColor(0, 0, 0, 192)
   love.graphics.rectangle('fill', x, y, w, h)
   love.graphics.setColor(255, 255, 255, 255)
   love.graphics.rectangle('line', x, y, w, h)
 
   love.graphics.print(tostring(love.timer.getFPS()), x + 2, y + 2)
+  love.graphics.print('Generations: ' .. counter, x + 2, y + 14)
 end
 
 function maze_generate(w, h)
@@ -62,18 +64,27 @@ function maze_generate(w, h)
   local out = {}
   for x = 1, w, 1 do
     for y = 1, h, 1 do
-      local visited = false
       local val = 1
-      if x == 1 or x == w or y == 1 or y == h then
-        visited = true
-        val = 0
-      end
-      print(visited)
-      out[index] = {x = x, y = y, val = val, visited = visited}
+      if x == 1 or x == w or y == 1 or y == h then val = 0 end
+      out[index] = {x = x, y = y, val = val}
       index = index + 1
     end
   end
   return out
+end
+
+function maze_init()
+  walllist = {}
+  maze = maze_generate(mazewidth, mazeheight)
+  local legit = false
+  while not legit do
+    start = math.random(#maze)
+    local this = maze[start]
+    if this.x > 2 and this.x < mazewidth - 2 and this.y > 2 and this.y < mazeheight - 2 then legit = true end
+  end
+  maze[start].val = 0
+  table_add_table(walllist, get_neighbors(start, 2, 1))
+  counter = counter + 1
 end
 
 function maze_draw(tbl)
